a40b533d0a4c3c2b946ec1aaa191f15b63cecef6
Zakir Sajib <zakirsajib@yahoo.co.uk>
2020-05-23 20:49:51 +0600

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,8 @@
 import React from "react"
-import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
   
