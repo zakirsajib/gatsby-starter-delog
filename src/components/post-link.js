da160306317a2440cbc04c8ac70e558892ae0d93
Zakir Hussain Sajib <zakirsajib@yahoo.co.uk>
2020-05-27 13:12:48 +0600

    no message

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -8,14 +8,6 @@ const PostLink = ({ post }) => (
         <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
       )}
     </Link>
-    <header>
-      <h2 className="post-title">
-        <Link to={post.frontmatter.path} className="post-link">
-          {post.frontmatter.title}
-        </Link>
-      </h2>
-      <div className="post-meta">{post.frontmatter.date}</div>
-    </header>
   </article>
 )
 export default PostLink
