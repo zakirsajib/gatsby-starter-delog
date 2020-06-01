b6851d1f1f73bff099346efce01b1b1ba33b4a65
Zakir Hussain Sajib <zakirsajib@yahoo.co.uk>
2020-06-01 14:50:15 +0600

    add new fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,12 @@
-import "./src/styles/global.scss"
+import './src/fonts/fonts.css'
+import './src/styles/global.scss'
 
 export const onServiceWorkerUpdateReady = () => {
     const answer = window.confirm(
       `This application has been updated. ` +
         `Reload to display the latest version?`
     )
-  
+
     if (answer === true) {
       window.location.reload()
     }
